84c1b26b21d1436acd1709e0c74f37e9b6cbbff2
Sebastian Nemak <devnull@der-basti.com>
2011-10-31 14:13:23 +0100

    Initial commit

---
+++ src/softwareentwicklung1/SoftwareEntwicklung1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwareentwicklung1;
+
+/**
+ *
+ * @author sne
+ */
+public class SoftwareEntwicklung1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
