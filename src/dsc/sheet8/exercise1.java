e238adf123d2a479030154fa5b83958896602a29
David Schwertfeger <david.schwertfeger@googlemail.com>
2011-11-18 20:52:51 +0100

    adding analog clock without design

---
+++ src/dsc/sheet8/exercise1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsc.sheet8;
+
+import StdLib.*;
+
+/**
+ *
+ * @author david
+ */
+public class exercise1 {
+
+    /*Schreiben Sie ein Programm, welches die folgenden Formen grafisch darstellt. 
+     *Wählen Sie eine geeignete Abstraktion zur Vereinfachung der Zeichnung, 
+     * z.B. können Sie ein Herz aus einer Verbindung von Rhombus und zwei Kreisen erstellen.
+     */
+    public static void main(String[] args) {
+           StdDraw.filledSquare(0.3, 0.4 , 0.1);
+           StdDraw.filledCircle(0.5, 0.5, .2);
+    }
+}
---
+++ src/dsc/sheet8/exercise2_AnalogueClock.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsc.sheet8;
+
+import StdLib.*;
+import java.util.Calendar;
+
+/**
+ *
+ * @author david
+ */
+public class exercise2_AnalogueClock {
+
+    /* Uhr. Schreiben Sie ein Programm, welches eine analoge Uhr mit Stunden, Minuten und Sekunden 
+     * anzeigt und aktualisiert. Benutzen Sie StdDraw.show(1000) um eine Pause von zirka einer 
+     * Sekunde zu erzeugen.
+     * 
+     * Tipp: Sie können den %-Operator auch mit Double-Werten verwenden.
+     */
+    public static void main(String[] args) {
+
+        double Xsecond = 0;
+        double Ysecond = 0;
+        double Xminute = 0;
+        double Yminute = 0;
+        double Xhour = 0;
+        double Yhour = 0;
+
+
+        double angle = 2 * Math.PI / 60;
+        double hourAngle = 2 * Math.PI / 12;
+
+
+
+
+        // rescale
+        StdDraw.setXscale(-1.0, 1.0);
+        StdDraw.setYscale(-1.0, 1.0);
+
+
+        while (true) {
+
+            Calendar calendar = Calendar.getInstance();
+
+            int hours = calendar.get(Calendar.HOUR);
+            int minutes = calendar.get(Calendar.MINUTE);
+            int seconds = calendar.get(Calendar.SECOND);
+
+            Xsecond = Math.sin(seconds * angle);
+            Ysecond = Math.cos(seconds * angle);
+
+            Xminute = Math.sin(minutes * angle) * 0.75;
+            Yminute = Math.cos(minutes * angle) * 0.75;
+
+            Xhour = Math.sin(hours * hourAngle) * 0.5;
+            Yhour = Math.cos(hours * hourAngle) * 0.5;
+
+            StdDraw.setPenColor(StdDraw.WHITE);
+            StdDraw.filledSquare(0, 0, 1);
+            StdDraw.setPenColor(StdDraw.BLACK);
+            StdDraw.line(0, 0, Xsecond, Ysecond);
+            StdDraw.line(0, 0, Xminute, Yminute);
+            StdDraw.line(0, 0, Xhour, Yhour);
+            StdDraw.show(1000);
+        }
+    }
+}
