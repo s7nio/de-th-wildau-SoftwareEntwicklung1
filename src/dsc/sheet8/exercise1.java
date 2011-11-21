42acb8c3c783d59ad7e149a8dfef136654f4d7ae
David Schwertfeger <david.schwertfeger@googlemail.com>
2011-11-21 19:31:59 +0100

    added ex. 1

--- src/dsc/sheet8/exercise1.java
+++ src/dsc/sheet8/exercise1.java
@@ -17,7 +17,47 @@ public class exercise1 {
      * z.B. können Sie ein Herz aus einer Verbindung von Rhombus und zwei Kreisen erstellen.
      */
     public static void main(String[] args) {
-           StdDraw.filledSquare(0.3, 0.4 , 0.1);
-           StdDraw.filledCircle(0.5, 0.5, .2);
+        
+        
+        // diamonds
+        double[] xdiamonds = {0.2, 0.35, 0.2, 0.05};
+        double[] ydiamonds = {0.4, 0.2, 0.0, 0.2};
+        
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.filledPolygon(xdiamonds, ydiamonds);
+        
+
+        // hearts
+        double[] xhearts = {0.8, 0.95, 0.8, 0.65};
+        double[] yhearts = {0.9, 0.7, 0.5, 0.7};
+        
+        StdDraw.filledPolygon(xhearts, yhearts);
+        StdDraw.filledCircle(0.725, 0.8, 0.123);
+        StdDraw.filledCircle(0.875, 0.8, 0.123);
+        
+        StdDraw.setPenColor(StdDraw.BLACK);
+        
+        
+        // spades
+        double[] xspades = {0.2, 0.3, 0.2, 0.1};
+        double[] yspades = {0.9, 0.75, 0.6, 0.75};
+        StdDraw.filledPolygon(xspades, yspades);
+
+        StdDraw.filledCircle(0.25, 0.675, 0.088);
+        StdDraw.filledCircle(0.15, 0.675, 0.088);
+        
+        double[] xspadestriangle = {0.05, 0.35, 0.2,};
+        double[] yspadestriangle = {0.5, 0.5, 0.58,};
+        StdDraw.filledPolygon(xspadestriangle, yspadestriangle);
+        
+        
+        // clubs
+        double[] xclubstriangle = {0.65, 0.95, 0.8,};
+        double[] yclubstriangle = {0.0, 0.0, 0.08,};
+        StdDraw.filledPolygon(xclubstriangle, yclubstriangle);
+        
+        StdDraw.filledCircle(0.88, 0.175, 0.088);
+        StdDraw.filledCircle(0.72, 0.175, 0.088);
+        StdDraw.filledCircle(0.8, 0.3, 0.088);
     }
 }
--- src/dsc/sheet8/exercise2_AnalogueClock.java
+++ src/dsc/sheet8/exercise2_AnalogueClock.java
@@ -32,9 +32,6 @@ public class exercise2_AnalogueClock {
         double angle = 2 * Math.PI / 60;
         double hourAngle = 2 * Math.PI / 12;
 
-
-
-
         // rescale
         StdDraw.setXscale(-1.0, 1.0);
         StdDraw.setYscale(-1.0, 1.0);
@@ -48,20 +45,24 @@ public class exercise2_AnalogueClock {
             int minutes = calendar.get(Calendar.MINUTE);
             int seconds = calendar.get(Calendar.SECOND);
 
-            Xsecond = Math.sin(seconds * angle);
-            Ysecond = Math.cos(seconds * angle);
+            Xsecond = Math.sin(seconds * angle) * 0.95;
+            Ysecond = Math.cos(seconds * angle) * 0.95;
 
             Xminute = Math.sin(minutes * angle) * 0.75;
             Yminute = Math.cos(minutes * angle) * 0.75;
 
-            Xhour = Math.sin(hours * hourAngle) * 0.5;
-            Yhour = Math.cos(hours * hourAngle) * 0.5;
+            Xhour = Math.sin(hours * hourAngle) * 0.6;
+            Yhour = Math.cos(hours * hourAngle) * 0.6;
 
             StdDraw.setPenColor(StdDraw.WHITE);
             StdDraw.filledSquare(0, 0, 1);
+            StdDraw.picture(0, 0, "clockface.png", 2.3, 2.3);
             StdDraw.setPenColor(StdDraw.BLACK);
+            StdDraw.setPenRadius(0.002);
             StdDraw.line(0, 0, Xsecond, Ysecond);
+            StdDraw.setPenRadius(0.005);
             StdDraw.line(0, 0, Xminute, Yminute);
+            StdDraw.setPenRadius(0.008);
             StdDraw.line(0, 0, Xhour, Yhour);
             StdDraw.show(1000);
         }
